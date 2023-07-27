21721f085a5127329c95231a44c52f481e6d90aa
antoKeinanen <anto.keinanen@gmail.com>
2023-07-27 14:48:17 +0300

    feat: allow vite to serve svgs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import wasm from "vite-plugin-wasm";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), wasm()],
+    plugins: [svgr(), react(), wasm()],
 });
