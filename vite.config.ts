f0534ad5f17fed64e5f08d3970194ba4c680701d
antoKeinanen <anto.keinanen@gmail.com>
2023-07-29 14:29:07 +0300

    ci: remove svg plugin from host config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import wasm from "vite-plugin-wasm";
-import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svgr(), react(), wasm()],
+    plugins: [react(), wasm()],
 });
