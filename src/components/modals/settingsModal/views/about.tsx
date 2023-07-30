766efe4ebba48828720d441848a3418f101b1085
antoKeinanen <anto.keinanen@gmail.com>
2023-07-30 16:49:46 +0300

    feat: display attributions in the settings

--- src/components/modals/settingsModal/views/about.tsx
+++ src/components/modals/settingsModal/views/about.tsx
@@ -1,5 +1,32 @@
 // @ts-expect-error ts(2307)
 import packageJson from "/package.json";
+// @ts-expect-error ts(2307)
+import licenses from "/licenses.json";
+
+export interface License {
+    name: string;
+    licenses: string;
+    repository: string;
+    licenseUrl: string;
+}
+
+export default function About() {
+    const credits: License[] = Object.entries(licenses).map(
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        ([name, body]: [string, any]) => ({
+            name: name,
+            licenses: body.licenses,
+            repository: body.repository,
+            licenseUrl: body.licenseUrl,
+        })
+    );
+
+    const creditsList = credits.map((credit) => (
+        <li key={"credit " + credit.name}>
+            <a className="underline" href={credit.repository}>{credit.name}</a> •{" "}
+            <a className="underline" href={credit.licenseUrl}>{credit.licenses}</a>
+        </li>
+    ));
 
     return (
         <div className="text-truegray-100">
@@ -9,6 +36,9 @@ import packageJson from "/package.json";
                 Version: {packageJson.version} • Engine:{" "}
                 {packageJson.dependencies.sedenion_engine}
             </p>
+            <hr className="-mx-1 my-4 border-truegray-400" />
+            <h2 className="text-2xl">Open source libraries used</h2>
+            <ul className="list-disc mt-2 ml-6">{creditsList}</ul>
         </div>
     );
 }
