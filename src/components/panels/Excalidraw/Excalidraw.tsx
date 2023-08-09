fa522fcf3acd92b77720b5230877e1ca57203b4f
antoKeinanen <anto.keinanen@gmail.com>
2023-08-09 09:51:48 +0300

    fix: set width and height

--- src/components/panels/Excalidraw/Excalidraw.tsx
+++ src/components/panels/Excalidraw/Excalidraw.tsx
@@ -1,5 +1,9 @@
 import { Excalidraw } from "@excalidraw/excalidraw";
 
 export default function ExcalidrawPanel() {
-    return <Excalidraw theme="dark"/>
+    return (
+        <div className="excalidraw-theme w-full h-full">
+            <Excalidraw theme="dark"/>
+        </div>
+    )
 }
