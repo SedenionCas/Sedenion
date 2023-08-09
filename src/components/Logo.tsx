9d32ca363d4b5a8dd8d1ebd693b8a9174a6fd0ab
antoKeinanen <anto.keinanen@gmail.com>
2023-08-09 11:52:46 +0300

    fix: add alt to logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import imgUrl from "/LogoS.svg?url";
 export default function Logo() {
     return (
         <span>
-            <img src={imgUrl}  className="h-14 w-14" />
+            <img src={imgUrl} alt="S" className="h-14 w-14" />
         </span>
     );
 }
