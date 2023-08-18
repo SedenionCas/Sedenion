3976ccc5271a701597da6c6c18aeb792dbc7725a
Anto Keinänen <62938747+antoKeinanen@users.noreply.github.com>
2023-08-18 16:50:22 +0300

    feat: better error handling (#29)
    * chore: update engine version
    
    * feat: display errors

--- src/components/panels/NumericEvaluator/NumericEvaluator.tsx
+++ src/components/panels/NumericEvaluator/NumericEvaluator.tsx
@@ -2,20 +2,28 @@ import { useState } from "react";
 import CalcBlock from "./CalcBlock";
 import { evaluate } from "sedenion_engine";
 import MathBlock from "../../MathBlock";
+import ErrorBlock from "./ErrorBlock";
 
 export default function NumericEvaluator() {
     const [calcBlocks, setCalcBlocks] = useState<JSX.Element[]>([]);
+    const [errorBlock, setErrorBlock] = useState<JSX.Element>(<></>);
     const [inputLatex, setInputLatex] = useState("");
     const [inputText, setInputText] = useState("");
 
     const handleCalculation = () => {
-        setCalcBlocks([
-            ...calcBlocks,
-            <CalcBlock
-                expression={inputLatex}
-                solution={evaluate(inputText)}
-            />,
-        ]);
+        try {
+            setCalcBlocks([
+                ...calcBlocks,
+                <CalcBlock
+                    expression={inputLatex}
+                    solution={evaluate(inputText)}
+                />,
+            ]);
+            setErrorBlock(<></>);
+        } catch (err: unknown) {
+            console.error(err);
+            setErrorBlock(<ErrorBlock content={err as string} />);
+        }
     };
 
     return (
@@ -23,7 +31,8 @@ export default function NumericEvaluator() {
             <div className="h-[calc(100vh-5rem)]">
                 {...calcBlocks}
 
-                <div className="mt-5 flex w-full justify-center">
+                <div className="mt-5 flex w-full flex-col items-center">
+                    {errorBlock}
                     <MathBlock
                         latex={inputLatex}
                         setLatex={setInputLatex}
