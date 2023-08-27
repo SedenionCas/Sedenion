a568f7edb4688d17171ed402a0e8db7f5b513fbc
antoKeinanen <anto.keinanen@gmail.com>
2023-08-27 14:35:31 +0300

    feat: update panel builder to support plugins

--- src/components/panels/PanelBuilder.tsx
+++ src/components/panels/PanelBuilder.tsx
@@ -1,29 +1,13 @@
-import React from "react";
-import LazySuspense from "../suspense/LazySuspense";
 import RenderWhenVisible from "@/util/RenderWhenVisible";
+import { usePluginStore } from "react-pluggable";
 
 export default function PanelBuilder() {
-    const LazyNumericEvaluator = React.lazy(
-        () => import("./NumericEvaluator/NumericEvaluator")
-    );
-    const LazyExcalidraw = React.lazy(() => import("./Excalidraw/Excalidraw"));
-    const LazyCas = React.lazy(() => import("./Cas/Cas"));
+    const pluginStore = usePluginStore();
+    const Renderer = pluginStore.executeFunction("Renderer.getRendererComponent");
 
     return {
-        basicCalc: () => (
-            <LazySuspense>
-                <LazyNumericEvaluator />
-            </LazySuspense>
-        ),
-        cas: () => (
-            <LazySuspense>
-                <LazyCas />
-            </LazySuspense>
-        ),
-        excalidraw: RenderWhenVisible(() => (
-            <LazySuspense>
-                <LazyExcalidraw />
-            </LazySuspense>
-        )),
+        basicCalc: () => <Renderer placement="Calculator.display"/>,
+        cas: () => <Renderer placement="CAS.display"/>,
+        excalidraw: RenderWhenVisible(() => <Renderer placement="Excalidraw.display"/>),
     };
 }
