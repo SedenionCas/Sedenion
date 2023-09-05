a17e393c9254cabc0c1c8e9a32e399c28174a121
antoKeinanen <anto.keinanen@gmail.com>
2023-09-05 19:14:41 +0300

    ref: add semantic styles to number input

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -17,7 +17,7 @@ function NumberInput({ value, onChange }: NumberInputProps) {
             }}
             value={content}
             type="number"
-            className="number-input appearance-none rounded-md border border-truegray-400 bg-truegray-700 px-1 py-0.5 text-truegray-100 shadow-none ring-0 focus:border-2 focus:border-blue-500 focus:shadow-none focus:outline-none focus:ring-0"
+            className="number-input appearance-none rounded-primary border border-border bg-truegray-700 px-1 py-0.5 text-text-1 shadow-none ring-0 focus:shadow-none focus:outline-none focus:ring-2 focus:ring-primary"
         />
     );
 }
