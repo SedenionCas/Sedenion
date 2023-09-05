69e42206b99a096071e813b77cf86084c1272eb9
antoKeinanen <anto.keinanen@gmail.com>
2023-09-05 19:14:31 +0300

    ref: add semantic styles to dropdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -9,17 +9,19 @@ type DropdownProps = {
 function Dropdown({ options, value, onChange }: DropdownProps) {
     const [content, setContent] = useState(value);
     const optionsComponents = options.map((option) => (
-        <option value={option}>{option}</option>
+        <option className="appearance-none" value={option}>
+            {option}
+        </option>
     ));
 
     return (
         <select
-            value={content} 
+            value={content}
             onChange={(e) => {
                 onChange(e.target.value);
                 setContent(e.target.value);
             }}
-            className="rounded-md border border-truegray-400 bg-truegray-700 px-1 py-0.5 text-truegray-100 shadow-none ring-0 focus:border-2 focus:border-blue-500 focus:shadow-none focus:outline-none focus:ring-0"
+            className="rounded-primary border border-border bg-surface-2 px-xs py-0.5 text-text-1 shadow-none ring-0 focus:ring-2 focus:ring-primary"
         >
             {optionsComponents}
         </select>
