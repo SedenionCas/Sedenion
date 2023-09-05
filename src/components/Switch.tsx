8178e3f7ac4b76e02dca6ba9c6a01a164b5df0d2
antoKeinanen <anto.keinanen@gmail.com>
2023-09-05 19:14:57 +0300

    ref: add semantic styles to switch

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -20,7 +20,7 @@ function Switch({ enabled, onClick }: SwitchProps) {
                     setChecked((last) => !last);
                     onClick();
                 }}
-                className="peer h-6 w-11 rounded-full bg-truegray-400  after:absolute  after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-green-300"
+                className="peer h-6 w-11 rounded-full bg-border  after:absolute  after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-border after:bg-text-1 after:transition-all after:content-[''] peer-checked:bg-primary peer-checked:after:translate-x-full peer-checked:after:border-text-2 peer-focus:ring-primary-dark"
             ></div>
         </label>
     );
